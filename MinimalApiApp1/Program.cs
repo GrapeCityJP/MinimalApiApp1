4813923ef7726a83a52109a3770322604fbc96ad
Kuni <kuni.ujiie@grapecity.com>
2022-04-11 13:42:05 +0900

    Add Endpoint for DTO.

--- MinimalApiApp1/Program.cs
+++ MinimalApiApp1/Program.cs
@@ -41,6 +41,26 @@ app.MapGet("/products/read/{id}", async (int id, TestDbContext testDb) =>
     return Results.Ok(product);
 });
 
+app.MapGet("/productsdto/read", async (TestDbContext testDb) =>
+{
+    var productsdto = await testDb.Products.Select(x => new ProductDto(x)).ToListAsync();
+
+    if (productsdto == null) return Results.NotFound();
+
+    return Results.Ok(productsdto);
+
+});
+
+app.MapGet("/productsdto/read/{id}", async (int id, TestDbContext testDb) =>
+{
+    var product = await testDb.Products.FindAsync(id);
+
+    if (product == null) return Results.NotFound();
+
+    return Results.Ok(new ProductDto(product));
+});
+
+
 app.Run();
 
 public class TestDbContext : DbContext
@@ -74,3 +94,18 @@ public class Product
     public Guid Rowguid { get; set; }
     public DateTime ModifiedDate { get; set; }
 }
+
+public class ProductDto
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; } = null!;
+    public string ProductNumber { get; set; } = null!;
+    public string? Color { get; set; }
+    public decimal ListPrice { get; set; }
+    public string? Size { get; set; }
+    public decimal? Weight { get; set; }
+
+    public ProductDto(Product product) =>
+    (ProductId, Name, ProductNumber, Color, ListPrice, Size, Weight) = (product.ProductId, product.Name, product.ProductNumber, product.Color, product.ListPrice, product.Size, product.Weight);
+}
+
